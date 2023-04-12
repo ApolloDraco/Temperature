22009d28c846d77eb2f9167dd7708d789c3b5f4c
Apollo <88677238+ApolloDraco@users.noreply.github.com>
2023-04-12 02:23:21 -0400

    Update Temperature.java

--- Temperature.java
+++ Temperature.java
@@ -1,13 +1,11 @@
 /* 
- *  Description: A Java Program for converting 35 degrees Celsius to Fahrenheit
-*/
+ *  Description: A Java program for converting 35 degrees Celsius to Fahrenheit
+ */
 
-public class Temperature  
- {  
-   public static void main (String args[])  
-    { float Fahrenheit, Celsius;  
-          Celsius= 35;  
-          Fahrenheit =((Celsius*9)/5)+32;  
-          System.out.println("Temperature in Fahrenheit is: "+Fahrenheit);  
-   }
+public class Temperature {
+    public static void main(String[] args) {
+        float celsius = 35;
+        float fahrenheit = (celsius * 9 / 5) + 32;
+        System.out.printf("%.2f degrees Celsius is equal to %.2f degrees Fahrenheit", celsius, fahrenheit);
+    }
 }
